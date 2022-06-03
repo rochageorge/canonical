52d428f41a98f15e775ef544731bf12ddcab3e3f
rochageorge <georgebezs@gmail.com>
2022-06-03 00:28:28 -0300

    update test

--- package_statistics_test.py
+++ package_statistics_test.py
@@ -15,8 +15,9 @@ def test_is_up_repository():
 @pytest.mark.parametrize("arch", ['amd64', 'udeb-amd64', 'udeb-all', 'udeb-amd64','udeb-arm64','udeb-armel','udeb-armhf', 'udeb-i386','udeb-mips64el', 'udeb-mipsel','udeb-ppc64el', 'udeb-s390x'])
 def test_get_links(arch):
     
-    has_link = len(mylib.get_links(url, arch))
-    assert has_link > 0
+    links = mylib.get_links(url, arch)
+    has_link = f'Contents-{arch}.gz' in links
+    assert has_link == True
 
 @pytest.mark.parametrize("arch, count", [('amd64', 138726), ('udeb-amd64', 2097), ('udeb-all', 1246), ('udeb-amd64', 2097), ('udeb-arm64', 2459), ('udeb-armel', 1647), ('udeb-armhf', 3195), ('udeb-i386', 2471), ('udeb-mips64el', 1801), ('udeb-mipsel', 1795), ('udeb-ppc64el', 1793), ('udeb-s390x', 1548)])
 def test_sum_per_arch(arch, count):
